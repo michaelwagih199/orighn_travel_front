d742f0f3df031e3b2dfe57be0f4de2c0589ac7eb
michaelwagih199 <michaelwagih199.com>
2025-02-21 22:43:28 +0200

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { WebsiteLayoutComponent } from './shared/layout/website-layout/website-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: WebsiteLayoutComponent,
+    providers: [
+      // AdminService,
+      // {provide: ADMIN_API_KEY, useValue: 12345},
+    ],
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./features/website/pages/home/home.component').then(
+            (mod) => mod.HomScreenComponent
+          ),
+      },
+    ]
+  },
+];
---
+++ src/app/features/website/pages/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-website-page-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: '../../website_style.css'
+})
+export class HomScreenComponent {
+  title = 'orighn_travel_front';
+}
